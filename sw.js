3b5c488054ddc90eb311987fa48f9176620439c7
alinicus-sudo <alinicus@gmail.com>
2026-09-13 08:57:31 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='name-draw-v3';
+const CACHE='fogarty-name-draw-v5-logo-embedded';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(
